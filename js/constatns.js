e89dbf512045ab4f15c99c635ccc5c8e4db0eafc
yanzi475 <2462761907@qq.com>
2017-09-07 12:14:16 +0800

    Update constatns.js

--- js/constatns.js
+++ js/constatns.js
@@ -1,9 +1,9 @@
 
 
 var constants = {
-    VIRTUAL_DOMAIN: 'www.google.com',
+    VIRTUAL_DOMAIN: 'www.google.com.hk',
     // 生产环境 下载页面（不能用https，因为页面里面的MTA脚本使用的是 http 协议）
-    DOWNLOAD_PAGE_URL: 'http://www.google.com',
+    DOWNLOAD_PAGE_URL: 'http://www.google.com.hk',
     // 测试环境 下载页面
     DOWNLOAD_PAGE_URL_FOR_TEST: '',
 };
