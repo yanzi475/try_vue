c75d34ab758c25b0a671c1919e77c5e214e23409
yanzi475 <2462761907@qq.com>
2017-09-07 10:26:59 +0800

    Update constatns.js

--- js/constatns.js
+++ js/constatns.js
@@ -1,11 +1,11 @@
 
 
 var constants = {
-    VIRTUAL_DOMAIN: 'www.webank.com',
+    VIRTUAL_DOMAIN: 'www.google.com',
     // 生产环境 下载页面（不能用https，因为页面里面的MTA脚本使用的是 http 协议）
-    DOWNLOAD_PAGE_URL: 'http://www.webank.com/app/common/bridge-download/index.html',
+    DOWNLOAD_PAGE_URL: 'http://www.google.com',
     // 测试环境 下载页面
-    DOWNLOAD_PAGE_URL_FOR_TEST: 'http://sit-hjdata.webank.com/k/common/bridge-download/index.html',
+    DOWNLOAD_PAGE_URL_FOR_TEST: '',
 };
 
 
